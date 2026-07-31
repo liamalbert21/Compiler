c58b358ff5101ca2573c3ebbacec645e669036a6
liamalbert21 <liam.stephen.albert@gmail.com>
2026-07-30 23:32:22 -0400

    Even more changes

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -12,6 +12,11 @@ Parser::Parser(std::vector<Token> tokens) :
 
 void Parser::generateAST() {
     m_ast = expression();
+    
+    if (!isEOF()) {
+        // Eventually, be more descriptive. Say something like: "ERROR: A group is closed when it never began!"
+        throw std::runtime_error("ERROR: Invalid syntax!");
+    }
 }
 
 void Parser::printAST() const {
@@ -21,6 +26,10 @@ void Parser::printAST() const {
 }
 
 std::optional<Token> Parser::matchTokens(std::initializer_list<Token::Type> types) {    
+    if (isEOF()) {
+        return std::nullopt;
+    }
+    
     Token target{ peek() };
     const auto it{ std::find(types.begin(), types.end(), target.type) };
 
@@ -36,42 +45,37 @@ std::unique_ptr<Expr> Parser::expression() {
 }
 
 std::unique_ptr<Expr> Parser::term() {
+    static constexpr std::initializer_list<Token::Type> types{ Token::Type::PLUS, Token::Type::MINUS };
     std::unique_ptr<Expr> expr{ factor() };
+    auto op{ matchTokens(types) };
 
-    while (!isEOF()) {
-        if (auto op{ matchTokens({ Token::Type::PLUS, Token::Type::MINUS }) }) {
-            expr = std::make_unique<Binary>(std::move(expr), op.value(), factor());
-        } else {
-            break;
-        }
+    while (op) {
+        expr = std::make_unique<Binary>(std::move(expr), op.value(), factor());
+        op = matchTokens(types);
     }
 
     return expr;
 }
 
 std::unique_ptr<Expr> Parser::factor() {
+    static constexpr std::initializer_list<Token::Type> types{ Token::Type::STAR, Token::Type::SLASH };
     std::unique_ptr<Expr> expr{ unary{}.right(*this) };
-    
-    while (!isEOF()) {
-        if (auto op{ matchTokens({ Token::Type::STAR, Token::Type::SLASH }) }) {
-            expr = std::make_unique<Binary>(std::move(expr), op.value(), unary{}.right(*this));
-        }
-        else {
-            break;
-        }
+    auto op{ matchTokens(types) };
+
+    while (op) {
+        expr = std::make_unique<Binary>(std::move(expr), op.value(), unary{}.right(*this));
+        op = matchTokens(types);
     }
 
     return expr;
 }
 
 std::unique_ptr<Expr> Parser::unary::right(Parser& parser) {
-    if (parser.isEOF()) {
-        return unary{}.left(parser);
-    }
+    static constexpr std::initializer_list<Token::Type> types{ Token::Type::MINUS };
 
     // Because these operators are right-associative, they requrire that we find the
     // operand AFTER the operator.
-    else if (auto op{ parser.matchTokens({ Token::Type::MINUS }) }) {
+    if (auto op{ parser.matchTokens(types) }) {
         return std::make_unique<Unary>(op.value(), unary{}.right(parser));
     }
 
@@ -79,35 +83,34 @@ std::unique_ptr<Expr> Parser::unary::right(Parser& parser) {
 }
 
 std::unique_ptr<Expr> Parser::unary::left(Parser& parser) {
+    static constexpr std::initializer_list<Token::Type> types{ Token::Type::FACTORIAL };
     
     // Because these operators are left-associative, they requrire that we find the
     // operand BEFORE the operator.
     std::unique_ptr<Expr> expr{ parser.primary() };
 
-    if (!parser.isEOF()) {
-        if (auto op{ parser.matchTokens({ Token::Type::FACTORIAL }) }) {
-            expr = std::make_unique<Unary>(op.value(), std::move(expr));
-        }
+    if (auto op{ parser.matchTokens(types) }) {
+        expr = std::make_unique<Unary>(op.value(), std::move(expr));
     }
 
     return expr;
 }
 
 std::unique_ptr<Expr> Parser::primary() {
+    std::unique_ptr<Expr> expr{ nullptr };
+
     if (auto num{ matchTokens({ Token::Type::INT, Token::Type::DOUBLE }) }) {
-        return std::make_unique<Primary>(num.value());
+        expr = std::make_unique<Primary>(num.value());
     }
     else if (auto grouping{ matchTokens({ Token::Type::LEFT_PAREN, Token::Type::LEFT_BRACK }) }) {
-        std::unique_ptr<Expr> expr{ std::make_unique<Grouping>(expression()) };
+        expr = std::make_unique<Grouping>(expression());
 
         // The next token MUST correspond to the group type
-        Token::Type end{ grouping.value().type == Token::Type::LEFT_PAREN ? Token::Type::RIGHT_PAREN : Token::Type::RIGHT_BRACK };
-        assert(matchTokens({ end }) && "ERROR: A group in the expression does not close!");
-        
-        return expr;
+        Token::Type expected{ grouping.value().type == Token::Type::LEFT_PAREN ? Token::Type::RIGHT_PAREN : Token::Type::RIGHT_BRACK };
+        assert(matchTokens({ expected }) && "ERROR: A group in the expression does not close!");
     }
-    
-    throw std::runtime_error("ERROR: Invalid syntax!");
+
+    return expr;
 }
 
 void Parser::advance() {
